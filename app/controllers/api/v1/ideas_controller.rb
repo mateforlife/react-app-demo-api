60d1f5b2e139fe8b9ac1e8cc3adda93917fde90c
MATEDORLIFE <RODRIGUEZ.RUIZ.ALEX@GMAIL.COM>
2018-10-16 15:13:17 -0300

    ideas update

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,5 +4,22 @@ module Api::V1
       @ideas = Idea.all
       render json: @ideas
     end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+
+    def update
+      @idea = Idea.find(params[:id])
+      @idea.update_attributes(idea_params)
+      render json: @idea
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
   end
 end
